5d31e96ad500ba4afcd27ea0478465eab290f6e3
byHYONS <byhyons@gmail.com>
2024-06-19 21:08:42 +0200

    [upd] moved the function into functions.php file and linked with index.php • Fine Esercizio

---
+++ functions.php
@@ -0,0 +1,60 @@
+<?php
+
+if (isset($_GET['password-length'])) {
+    $password_length = intval($_GET['password-length']);
+    $password = generatePassword($password_length);
+    header("Location: index.php?password=" . urlencode($password));
+    exit();
+}
+
+// Funzione principale per generare password
+function generatePassword($password_length)
+{
+    // Variabili
+    $alpha = 'abcdefghijklmnopqrstuvwxyz';
+    $num = '0123456789';
+    $simbol = '@!?#$';
+
+    // Array alfanumerico
+    $alpha_num_array = genAlphaNumArray($alpha, $num, $simbol);
+
+    // Password generata
+    return genPassword($password_length, $alpha_num_array);
+}
+
+// Funzione per creare la password
+function genPassword($length, $characters)
+{
+    $length_char = count($characters);
+    $random_password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $character = $characters[rand(0, $length_char - 1)];
+        // Creo maiuscole random
+        if (!is_numeric($character) && rand(0, 1) === 1) {
+            $character = strtoupper($character);
+        }
+        $random_password .= $character;
+    }
+
+    return $random_password;
+}
+
+// Funzione per creare array di lettere, numeri e simboli
+function genAlphaNumArray($alpha, $num, $simbol)
+{
+    $list = [];
+    // Ciclo lettere
+    for ($i = 0; $i < strlen($alpha); $i++) {
+        $list[] = $alpha[$i];
+    }
+    // Ciclo numeri
+    for ($i = 0; $i < strlen($num); $i++) {
+        $list[] = $num[$i];
+    }
+    // Ciclo simboli
+    for ($i = 0; $i < strlen($simbol); $i++) {
+        $list[] = $simbol[$i];
+    }
+    return $list;
+}
--- index.php
+++ index.php
@@ -1,64 +1,6 @@
 <?php
-// sessione:
-// session_start();
-// var_dump($_SESSION);
-
-//? input lunghezza password:
-$password_length = intval($_GET['password-length']);
-//// var_dump($password_length);
-
-//? variabili:
-$alpha = 'abcdefghijklmnopqrstuvwxyz';
-$num = '0123456789';
-$simbol= '@!?#$';
-
-//? array alfanumerico:
-$alpha_num_array = genAlphaNumArray($alpha, $num, $simbol);
-//// var_dump($alpha_num_array);
-
-//? password generata:
-$password = genPassword($password_length, $alpha_num_array);
-echo $password;
-
-
-//* funzione principale per generare password:
-function genPassword($length, $characters) {
-
-    $length_char = count($characters);
-    $random_password = '';
-
-    for($i=0; $i<$length; $i++) {
-        $character = '';
-        $character .= $characters[rand(0, $length_char - 1)];
-        // creo maiuscole random:
-        if (!is_numeric($character) && rand(0, 1) === 1) {
-            $character = strtoupper($character);
-        }
-        $random_password .= $character;
-    }
-
-    return $random_password;
-};
-
-//* funzione per creare array di lettere, numeri e simboli:
-function genAlphaNumArray($alpha, $num, $simbol) {
-    $list = [];
-    // ciclo lettere, numeri e simboli:
-    for ($i=0; $i < strlen($alpha) ; $i++) { 
-       $list[] = $alpha[$i]; 
-    };
-    for ($i=0; $i < strlen($num) ; $i++) { 
-       $list[] = $num[$i]; 
-    };
-    for ($i=0; $i < strlen($simbol) ; $i++) { 
-       $list[] = $simbol[$i]; 
-    };
-    return $list;
-};
-
-
+$password = isset($_GET['password']) ? $_GET['password'] : null;
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -67,9 +9,9 @@ function genAlphaNumArray($alpha, $num, $simbol) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
-    <!-- boostrap -->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- css -->
+    <!-- CSS -->
     <link rel="stylesheet" href="./style.css" type="text/css">
 </head>
 
@@ -78,24 +20,28 @@ function genAlphaNumArray($alpha, $num, $simbol) {
         <h1 class="text-center p-3">Password Generator</h1>
     </header>
     <main>
-        <!-- form per inviare password -->
         <div class="container">
-            <form action="index.php" method="GET">
-                <div class="mb-3 col-5 my-5">
-                    <label for="password-length" class="form-label">Stabilisci lunghezza password</label>
-                    <input type="number" class="form-control" id="password-length" name="password-length" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">Enter the length of the password to be generated</div>
+            <?php if ($password) : ?>
+                <h3 class="text-center">La password generata è:</h3>
+                <div class="alert alert-info mt-5" role="alert">
+                    <p class="text-center "><?php echo $password; ?></p>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <?php else : ?>
+                <form action="functions.php" method="GET">
+                    <div class="mb-3 col-5 my-5">
+                        <label for="password-length" class="form-label">Stabilisci lunghezza password</label>
+                        <input type="number" class="form-control" id="password-length" name="password-length" aria-describedby="passwordHelp">
+                        <div id="passwordHelp" class="form-text">Enter the length of the password to be generated</div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            <?php endif; ?>
         </div>
-
     </main>
 
     <footer class="text-center p-3">
         ©byHYONS™
     </footer>
-
 </body>
 
 </html>
